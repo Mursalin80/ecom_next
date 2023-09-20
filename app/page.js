933a1b8389bc192f65182756d174fd46913a568f
Mursalin <mursalin80@hotmail.com>
2023-09-20 11:56:35 +0500

    some inhancements in main page.js

--- app/page.js
+++ app/page.js
@@ -18,7 +18,10 @@ export default function Home() {
   const productsQuery = async ({ take = 10, lastCursor = "" }) => {
     setLoading(true);
     let url = "/api/products?" + new URLSearchParams({ take, lastCursor });
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      cache: "force-cache",
+      next: { revalidate: 3600 },
+    });
     let data = await response.json();
     setLoading(false);
     if (data) {
@@ -56,13 +59,12 @@ export default function Home() {
         class="bg-teal-500 hover:bg-blue-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
       >
         <svg
-          class="fill-current w-4 h-4 mr-2"
+          class={`fill-current w-5 h-5 mr-2 animate-bounce bg-gradient-to-b from-blue-200 to-blue-600 `}
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
         >
           <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
         </svg>
-
         <span>Load more products!</span>
       </button>
 
--- components/util/Spinner.jsx
+++ components/util/Spinner.jsx
@@ -1,8 +1,10 @@
 const Spinner = () => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center relative w-full">
+      <div className="absolute -top-20 -left-4 w-36 h-36 bg-blue-200 filter blur-xl mix-blend-multiply opacity-50"></div>
+      <div className="absolute -top-20 -right-4 w-36 h-36 bg-yellow-200 filter blur-xl mix-blend-multiply opacity-50"></div>
       <div
-        className="inline-block m-1 p-1 h-12 w-12 text-6xl animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        className="inline-block m-1 p-1 h-8 w-8 text-6xl animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_3s_linear_infinite]"
         role="status"
       >
         <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)] ">
@@ -10,7 +12,15 @@ const Spinner = () => {
         </span>
       </div>
       <div
-        className="inline-block h-8 w-8 m-1 p-1 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        className="inline-block h-6 w-6 m-1 p-1 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_2s_linear_infinite]"
+        role="status"
+      >
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)] ">
+          Loading...
+        </span>
+      </div>
+      <div
+        className="inline-block h-4 w-4 m-1 p-1 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
         role="status"
       >
         <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)] ">
